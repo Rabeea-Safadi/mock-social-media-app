e2ef5a4afb96c0a502e6fe6b689d567397ebfcf2
Rabeea Safadi <rabeeasafadi@gmail.com>
2022-08-21 16:49:28 +0300

    added mock data to home and about pages

--- src/components/AboutPage/About.jsx
+++ src/components/AboutPage/About.jsx
@@ -1,13 +1,29 @@
 export default function About(props) {
   return (
-    <p>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos corrupti
-      facere vitae. Fugit dolorem ab temporibus aspernatur sit, unde debitis
-      laudantium voluptatibus! Debitis, quam. Aliquid modi, veritatis quo
-      numquam omnis illum ipsam eius velit enim possimus! Asperiores explicabo
-      alias sapiente harum quod fugit facilis officia, laborum nobis doloribus!
-      Rerum dolore cum quos mollitia sit id quis quibusdam incidunt inventore
-      sunt.
-    </p>
+    <>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos corrupti
+        facere vitae. Fugit dolorem ab temporibus aspernatur sit, unde debitis
+        laudantium voluptatibus! Debitis, quam. Aliquid modi, veritatis quo
+        numquam omnis illum ipsam eius velit enim possimus! Asperiores explicabo
+        alias sapiente harum quod fugit facilis officia, laborum nobis
+        doloribus! Rerum dolore cum quos mollitia sit id quis quibusdam incidunt
+        inventore sunt.
+      </p>
+      <hr />
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias,
+        exercitationem. Dolores eaque accusantium impedit temporibus ducimus?
+        Cum incidunt veniam molestias! Velit enim quam vero, voluptates atque
+        blanditiis facere assumenda iste! Eaque accusamus dolor possimus! Quasi
+        debitis quas deleniti tempore minima. Placeat, eveniet. Ad unde, quam
+        voluptates labore dicta corporis ducimus? A, fugiat earum numquam
+        aperiam eaque ipsam cum assumenda quae? Similique molestiae itaque saepe
+        quo fugit cumque distinctio vero obcaecati? Omnis ipsum expedita sed.
+        Enim voluptas delectus soluta labore sapiente! Quod nam rem ullam
+        voluptatum quos minus dolore tenetur eum! Doloribus natus voluptatibus
+        odit aliquid fuga optio minus ullam possimus.
+      </p>
+    </>
   );
 }
--- src/components/HomePage/Home.jsx
+++ src/components/HomePage/Home.jsx
@@ -15,6 +15,43 @@ const Home = (props) => {
         dolores maiores perspiciatis, voluptatibus culpa ducimus! Tempore
         voluptate eaque nesciunt, eveniet hic est iure pariatur sapiente.
       </p>
+      <hr />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus,
+        corrupti! Corporis, recusandae at est sapiente maiores ex voluptatibus
+        ullam explicabo? Architecto delectus nesciunt sint. Libero eligendi
+        omnis dignissimos fuga voluptate! Eum voluptatum praesentium error
+        voluptatibus debitis nemo nesciunt, eligendi asperiores! Blanditiis
+        fuga, commodi doloremque possimus ad qui nisi culpa quibusdam? Laborum
+        corporis quos dolor sint ipsum deserunt, rerum nesciunt error. Dolor
+        itaque placeat molestiae! Aspernatur magnam maxime possimus aliquam
+        perspiciatis? Hic, temporibus repudiandae iusto quasi iste molestias nam
+        minus rem. Dolorem iure ab fugiat et quia, maiores doloremque laboriosam
+        aspernatur. Asperiores corrupti aut excepturi id odio dolor eos ipsam
+        suscipit. Quibusdam a ducimus at labore, vitae facere expedita sed
+        deleniti. Facere, iste neque. Incidunt omnis perspiciatis repellendus
+        velit culpa. Ea. Repellendus nostrum, pariatur voluptatum nemo debitis
+        maiores eius ipsam suscipit. Harum dicta voluptate asperiores rerum
+        excepturi totam consectetur cum veniam? Aliquam, quasi? Harum
+        exercitationem sed consectetur voluptates laudantium id! Laudantium.
+        Mollitia nam fugit asperiores vitae minima corrupti numquam, cupiditate
+        aperiam? Labore sint mollitia incidunt hic eius magnam ea reprehenderit
+        ab. Doloremque in eos perferendis nisi quis repudiandae corrupti
+        pariatur adipisci. Minima explicabo sint dignissimos soluta deserunt
+        vero, commodi est odio? Earum autem laboriosam facilis eius, pariatur
+        harum debitis nobis magni? Quas asperiores vel rem repellendus deserunt
+        cum sapiente natus voluptate? Illum eaque incidunt culpa repellat dicta
+        soluta a, assumenda sequi. Eius ab necessitatibus harum incidunt
+        consequuntur tenetur nisi voluptatibus aut. Ad doloribus, laudantium
+        asperiores architecto natus quae quas nesciunt accusantium? Recusandae
+        sunt quo illo unde ipsa eaque, eum omnis! Ipsa. Tempore quidem iste
+        vitae qui maiores quam. Aspernatur, ipsa pariatur. Impedit fuga sequi
+        animi, libero deleniti reiciendis eligendi velit natus. Sapiente
+        accusamus repudiandae illum enim consectetur officiis quaerat
+        reprehenderit beatae. Quaerat mollitia illo sunt officiis asperiores sit
+        facilis veritatis debitis. Tenetur blanditiis soluta, rerum odio est
+        optio dolorem magni maiores.
+      </p>
     </>
   );
 };
